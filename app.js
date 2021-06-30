4b4dabf83a452a7c196c127cd8e1e31cf8db081a
Akshil V Thumar <75160883+AkshilVT@users.noreply.github.com>
2021-06-30 18:54:05 +0530

    Login ed2

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 import express from "express";
-import { urlencoded } from "body-parser";
+import pkg from 'body-parser';
+const { urlencoded } = pkg;
 import ejs from "ejs";
 import mongoose from "mongoose";
 
@@ -16,7 +17,7 @@ app.use(express.static(__dirname + "/public/"));
 
 
 app.get("/", function(req, res){
-    res.render("index");
+    res.render("Login");
 });
 
 app.listen(3000, function(){
