ad66c7e27d052711d99ecc997712a74d0e953635
kavania2002 <72624080+kavania2002@users.noreply.github.com>
2021-06-30 18:35:49 +0530

    Add starting of node

---
+++ app.js
@@ -0,0 +1,24 @@
+import { fileURLToPath } from "url";
+import path from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+import express from "express";
+import { urlencoded } from "body-parser";
+import ejs from "ejs";
+import mongoose from "mongoose";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(urlencoded({extended:true}));
+app.use(express.static(__dirname + "/public/"));
+
+
+app.get("/", function(req, res){
+    res.render("index");
+});
+
+app.listen(3000, function(){
+    console.log("Server started at port 3000");
+});
